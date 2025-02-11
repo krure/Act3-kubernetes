eb89be39bd24f960fc3a8129a9641ed991d83283
krure <krurecaliomat@gmail.com>
2025-02-10 20:14:31 -0600

    Added Secret handling

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import config
+import os
 from flask_mysqldb import MySQL
 from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = config.HEX_SEC_KEY
-app.config['MYSQL_HOST'] = config.MYSQL_HOST
-app.config['MYSQL_USER'] = config.MYSQL_USER
-app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
-app.config['MYSQL_DB'] = config.MYSQL_DB
+app.config['SECRET_KEY'] = os.environ.get('HEX_SEC_KEY')
+app.config['MYSQL_HOST'] =  os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] =  os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] =  os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] =  os.environ.get('MYSQL_DB' )
 
 mysql = MySQL(app)
 
--- dockerfile
+++ dockerfile
@@ -1,16 +1,12 @@
 FROM python:3.7
 ARG NGROK_AUTHTOKEN
-ENV ENV_NGROK_AUTHTOKEN $NGROK_AUTHTOKEN
+
 RUN mkdir /app
 EXPOSE 80
 WORKDIR /app/
 ADD . /app/
+
 RUN pip3 install -r requirements.txt
 RUN pip3 install flask_mysqldb
-RUN curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
-|  tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
-&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
-|  tee /etc/apt/sources.list.d/ngrok.list \
-&&  apt update \
-&&  apt install ngrok
+
 CMD python /app/app.py
